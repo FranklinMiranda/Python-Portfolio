f43e91f4bf007401f2d16825bf021218bab8a483
FranklinMiranda <fdm61344@gmail.com>
2022-12-02 18:04:14 -0500

    CM

--- python.py
+++ python.py
@@ -3,3 +3,35 @@ print("Starting Python")
 msg = "Hello World"
 
 print(msg)
+
+if 5 > 2:
+    print("Five is greater than two")
+
+x = 10
+y = 20
+
+if x > y:
+    print("X is greater than Y")
+else:
+    print("Y is greater than X")
+
+
+#This is a python comment
+
+# Variables do  not have a declaration keyword in python
+z = 30
+
+# Casting can be used to declare a specific datatype with variable declarations 
+a = str(3)
+b = int(5)
+c = float(10)
+
+print(a,b,c)
+
+# Type() function can be used to get the type of the variable 
+print(type(a))
+
+A = str(40)
+print(A)
+
+# Variable Name 
