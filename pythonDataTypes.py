e00f8e172507fb27d8fa8fc24f9290628f02bee1
FranklinMiranda <fdm61344@gmail.com>
2022-12-02 18:55:22 -0500

    Cm

--- pythonDataTypes.py
+++ pythonDataTypes.py
@@ -7,6 +7,7 @@
 x = "String"
 print(type(x))
 
+# Python Data Types 
 x = "Hello World" # Str Data Type
 x = 20 # Int Data Type
 x = 20.5 # Float Data Type
@@ -22,3 +23,19 @@ x = b"Hello" # Bytes Data Type
 x = bytearray(5) # Bytearray Data Type
 x = memoryview(bytes(5)) # Memoryview Data Type
 x = None # NoneType Data Type
+
+# Python Data Types assigned using Casting
+x = str("Hello World") # Str Data Type
+x = int(20) # Int Data Type
+x = float(20.5) # Float Data Type
+x = complex(1j) # Complex Data Type
+x = list(["Apples", "Bananas", "Cherries"]) # List Data Type
+x = tuple(("Apples", "Bananas", "Cherries")) # Tuple Data Type
+x = range(6) # Range Data Type
+x = dict({"name" : "Franklin", "age": 24}) # Dictionary Data Type
+x = set({"Apples", "Bananas", "Cherries"}) # Set Data Type
+x = frozenset({"Apples", "Bananas", "Cherries"}) # Frozen Set Data Type
+x = bool(True) # Boolean Data Type
+x = bytes(5) # Bytes Data Type
+x = bytearray(5) # Bytearray Data Type
+x = memoryview(bytes(5)) # Memoryview Data Type
