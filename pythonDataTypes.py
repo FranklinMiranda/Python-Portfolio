62fe7d994d408495f3a82104f8cada52a62a2552
FranklinMiranda <fdm61344@gmail.com>
2022-12-02 18:51:38 -0500

    Cm

--- pythonDataTypes.py
+++ pythonDataTypes.py
@@ -7,3 +7,18 @@
 x = "String"
 print(type(x))
 
+x = "Hello World" # Str Data Type
+x = 20 # Int Data Type
+x = 20.5 # Float Data Type
+x = 1j # Complex Data Type
+x = ["Apples", "Bananas", "Cherries"] # List Data Type
+x = ("Apples", "Bananas", "Cherries") # Tuple Data Type
+x = range(6) # Range Data Type
+x = {"name" : "Franklin", "age": 24} # Dictionary Data Type
+x = {"Apples", "Bananas", "Cherries"} # Set Data Type
+x = frozenset({"Apples", "Bananas", "Cherries"}) # Frozen Set Data Type
+x = True # Boolean Data Type
+x = b"Hello" # Bytes Data Type
+x = bytearray(5) # Bytearray Data Type
+x = memoryview(bytes(5)) # Memoryview Data Type
+x = None # NoneType Data Type
