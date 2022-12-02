41aef0218c57e396dbde14951152911563d2cd24
FranklinMiranda <fdm61344@gmail.com>
2022-12-02 18:59:15 -0500

    Cm

---
+++ pythonNumbers.py
@@ -0,0 +1,10 @@
+# Python Numbers
+# Authorship: Franklin Miranda
+# Date: 12/2/2022
+# This file lists and explores the different Python Number Data Types
+
+x = 1 # Integer Data Number Type
+y = 1.5 # Float Data Number Type
+z = 1j # Complex Data Number Type
+
+print(x,y,z)
