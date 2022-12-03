0d5e47f801ab0addc18b72c017587cdfbb9dd1f5
FranklinMiranda <fdm61344@gmail.com>
2022-12-03 16:06:30 -0500

    Cm

--- pythonLists.py
+++ pythonLists.py
@@ -4,3 +4,14 @@
 # This file lists and explores the Python Lists Data Type
 
 # Lists are created using square brackets []
+myList = ["Apples", "Bananas", "Cherries"]
+print(myList)
+print(myList[0])
+print(myList[-1])
+print(len(myList))
+
+boolList = [True, False, True]
+print(boolList)
+
+multiDataList = ["Apples", 1, True]
+print(multiDataList)
