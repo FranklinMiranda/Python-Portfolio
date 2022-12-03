fff70bcb7b372555abadeb80f44d0f68aa71b863
FranklinMiranda <fdm61344@gmail.com>
2022-12-03 15:23:55 -0500

    Cm

--- pythonBoolean.py
+++ pythonBoolean.py
@@ -2,3 +2,9 @@
 # Authorship: Franklin Miranda
 # Date: 12/3/2022
 # This file lists and explores the Python Boolean Data Type
+
+if 10 > 20:
+    print("Value 1 is greater than value 2")
+else: 
+    print("Value 2 is greater than value 1")
+
