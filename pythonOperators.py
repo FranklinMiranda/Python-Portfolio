c99bb5ccab974bcb510adfeb1a66da7168b33876
FranklinMiranda <fdm61344@gmail.com>
2022-12-03 15:28:55 -0500

    Cm

--- pythonOperators.py
+++ pythonOperators.py
@@ -3,3 +3,13 @@
 # Date: 12/3/2022
 # This file lists and explores the different Python Operators
 
+x = 5
+y = 6
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
