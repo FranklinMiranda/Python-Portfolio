ab7a14a971ee1196ed9bacbd121caa22042edd26
FranklinMiranda <fdm61344@gmail.com>
2022-12-02 19:15:20 -0500

    CM

--- pythonStrings.py
+++ pythonStrings.py
@@ -2,3 +2,19 @@
 # Authorship: Franklin Miranda
 # Date: 12/2/2022
 # This file lists and explores the different aspects of the Python String Data Type
+
+string = "This is a string"
+print(string)
+
+# Multiline Strings can be assigned using 3 quotation marks
+a = """This is 
+a Multiline
+String."""
+print(a)
+
+# String Characters can be accessed using indexes because strings are Arrays
+print(string[1], string[10])
+
+# Strings can be Looped through using for Loops 
+for x in string:
+    print(x)
