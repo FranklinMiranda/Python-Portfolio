5f24817c326d3c5cfe9aa25c7f3509350b738366
FranklinMiranda <fdm61344@gmail.com>
2023-03-22 16:36:50 -0400

    Update to Practice

--- pythonPractice.py
+++ pythonPractice.py
@@ -133,7 +133,7 @@ print(thisList)
 thisList.reverse()
 print(thisList)
 
-#Coping a List, Lists can be Copied in 2 ways
+#Copying a List, Lists can be Copied in 2 ways
 listCopy1 = thisList.copy()
 listCopy2 = list(thisList)
 
@@ -240,3 +240,284 @@ thisSet.update(thisFruitSet)
 
 print(thisSet)
 
+#Removing Items from a set
+thisSet.remove("mangos")
+thisSet.discard("papayas")
+
+print(thisSet)
+
+#Clearing or deleting sets
+thisSet.clear()
+
+print(thisSet)
+
+del thisSet
+
+
+#Joining Sets
+set1 = {1,2,3,4,5,6,7,8}
+set2 = {5,6,7,8,9,0}
+
+set3 = set1.union(set2)
+print(set3)
+
+set1.update(set2)
+print(set1)
+
+#Intersection Update updates sets keeping items present only in both sets
+set1 = {1,2,3,4,5,6,7,8}
+set2 = {5,6,7,8,9,0}
+
+set1.intersection_update(set2)
+print(set1)
+
+set3 = set1.intersection(set2)
+print(set3)
+
+#Symmetric Difference Update keeps only items that are not present in both sets
+set1 = {1,2,3,4,5,6,7,8}
+set2 = {5,6,7,8,9,0}
+
+set1.symmetric_difference_update(set2)
+print(set1)
+
+set1 = {1,2,3,4,5,6,7,8}
+set2 = {5,6,7,8,9,0}
+
+set3 = set1.symmetric_difference(set2)
+print(set3)
+
+
+#Dictionaries
+thisDict = {"fname": "Franklin", "lname": "Miranda"}
+
+print(thisDict)
+print(len(thisDict))
+
+#Accessing Dictionaries
+print(thisDict['lname'])
+
+#Get method can also be used to retrieve a dictionary key value by key
+x = thisDict.get("lname")
+print(x)
+
+#Get a list of all dictionary key, updates after changes to the dictionary
+y = thisDict.keys()
+print(y)
+
+thisDict['age'] = 24
+
+print(y)
+
+#Get a list of all values of a dictionary, updates after changes to the dictionary
+z = thisDict.values()
+print(z)
+
+thisDict['height'] = 75
+
+print(z)
+
+#Get a list of all key-value pairs in a dictionary as tuples in a list, updates after changes to the dictionary
+x = thisDict.items()
+print(x)
+
+#Check if key exists in a dictionary
+if "lname" in thisDict:
+    print(True)
+
+#Changing or Add values in dictionary
+thisDict["age"] = 25
+print(thisDict["age"])
+
+#Updating the dictionary with a key-value pair
+thisDict.update({"birthyear": 1998})
+print(thisDict['birthyear'])
+
+#Removing items from a dictionary
+thisDict.pop("birthyear")
+
+print(thisDict)
+
+del thisDict['height']
+
+print(thisDict)
+
+thisDict.clear()
+print(thisDict)
+
+thisDict = {"fname": "Franklin", "lname": "Miranda"}
+
+
+#Looping through a Dictionary
+for x in thisDict:
+    print(x)
+
+for x in thisDict:
+    print(thisDict[x])
+
+#Copying a Dictionary 
+myDict = thisDict.copy()
+print(myDict)
+
+#Nested Dictionary
+myFamily = {"person1" : {"fname": "Franklin", "lname": "Miranda"},"person2" : {"fname": "Anna", "lname": "Miranda"} }
+
+#Accessing items in a Nested Dictionary
+print(myFamily['person1']['fname'])
+
+
+#If ELIF ELSE Conditional Statements
+a = 10
+b = 20
+c = 30
+
+if a > b: 
+    print("a is greater than b")
+elif a > c:
+    print("a is greater than c")
+elif b > c:
+    print("b is greater than c")
+else: 
+    print("c is greater than a and b")
+
+#Short Hand If Statement 
+if b > a: print("b is greater than a")
+
+#Short Hand If Else
+print("c is greater than b") if c > b else print("b is greater than c")
+
+
+#And operator with IF ELIF ELSE statements
+if a < b and b < c:
+    print("Both conditions are True")
+
+#Or operator with IF ELIF ELSE statements
+if a > b or c > a:
+    print("At least one of the conditions is True")
+
+#Not operator, used to reverse the result of the conditional statement 
+if not a > b:
+    print("b is greater than a")
+
+#Nested IF statements
+if c > b:
+    print("c is greater than b")
+    if b > a: 
+        print("b is greater than a")
+    else:
+        print("Neither c is greater than b nor b is greater than a")
+
+#Use a pass statement with a empty If statement to avoid a error 
+if b > a:
+    pass
+
+#While Loops, can also use Break and Continue Statements to break out of a loop or to skip a loop
+i = 0
+while i < 6:
+    print(i)
+    i = i + 1
+else: 
+    print("i is no longer less than 6")
+
+#For Loops, can also use Break and Continue Statements to break out of a loop or to skip a loop
+numbers = [1,2,3,4,5,6,7,8,9]
+fruits = ["apples", "bananas"]
+
+for n in numbers:
+    print(n)
+else:
+    print("Loop Finished")
+
+#Nested Loops
+for n in numbers:
+    for x in fruits:
+        print(n, x)
+
+#Pass statement is used to avoid error on empty loop block
+for x in [0,1,2]:
+    pass
+
+#Functions 
+def messagePrinter(message):
+    print("Hello " + message)
+
+messagePrinter("World")
+
+
+#Passing arbitrary arguments to functions 
+def familyFunc(*kids):
+    print("The youngest child is " + kids[0])
+
+familyFunc('Franklin', 'Anna')
+
+#Default Parameter Values 
+def countryPrinter(country = "Norway"):
+    print("I am from " + country)
+
+countryPrinter()
+
+
+#Function return 
+def calculator(n):
+    return n + 1
+
+print(calculator(4))
+
+#Lambda Functions 
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(x(1,2,3))
+
+#Using a lambda function inside another function
+def cal(n):
+    return lambda a : a * n
+
+doubler = cal(2)
+print(doubler(10))
+
+#Classes
+class MyClass:
+    x = 5
+
+#Creating Objects from a Class
+obj1 = MyClass()
+print(obj1.x)
+
+#Class _init_ function and __str__ function
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def myFunc(self):
+        print("Hello my name is " + self.name)
+
+    def __str__(self):
+        return f"{self.name}({self.age})"
+
+person1 = Person("Franklin", 24)
+
+
+print(person1.name)
+print(person1.age)
+print(person1)
+person1.myFunc()
+
+
+
+#Inheritance
+class Student(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+        self.fname = fname
+        self.lname = lname
+
+x = Student("Anna", 'Miranda')
+print(x.fname)
+print(x.lname)
